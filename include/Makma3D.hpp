e1bbcaf2df52aa5887c67f60b30affbf89c0844c
Lut99 <mullertim99@gmail.com>
2021-10-04 18:44:44 +0200

    Created general header file for the engine lib

---
+++ include/Makma3D.hpp
@@ -0,0 +1,16 @@
+/* MAKMA 3D.hpp
+ *   by Lut99
+ *
+ * Created:
+ *   04/10/2021, 16:27:45
+ * Last edited:
+ *   04/10/2021, 16:27:45
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   The main header file for the Makma3D library. Contains includes for
+ *   everything needed for the basic experience.
+**/
+
+
