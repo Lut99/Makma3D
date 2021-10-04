9854a2c4ffe30db4faa7c3e651be505ec1791d7a
Lut99 <mullertim99@gmail.com>
2021-10-04 23:07:12 +0200

    Trying cmake a bit

---
+++ test/Main.cpp
@@ -0,0 +1,13 @@
+/* MAIN.cpp
+ *   by Lut99
+ *
+ * Created:
+ *   04/10/2021, 22:43:17
+ * Last edited:
+ *   04/10/2021, 22:43:17
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Test file with which we can test the library.
+**/
